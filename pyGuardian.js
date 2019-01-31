17c0ac1bef059e6b827f43666a4a828cdd52f23b
Brian Osgood <35780331+osgoodbl@users.noreply.github.com>
2019-01-30 20:53:20 -0800

    Changed Page Layout

--- pyGuardian.js
+++ pyGuardian.js
@@ -21,7 +21,7 @@
         schemaCallback([tableSchema]);
     };
 
-var apiKey = "668b2e3fc37a47de87d3b5e29defacd9";
+var apiKey = "9a8e500b4d154f899dfea96fc3c4889d";
 
     // Download the data
     myConnector.getData = function(table, doneCallback) {
@@ -39,7 +39,7 @@ var apiKey = "668b2e3fc37a47de87d3b5e29defacd9";
             for (var i = 0, len=feat.length; i<len; i++){
 
                 tableData.push({
-                    "gamertag": feat[i].ErrorStatus,
+                    "gamertag": feat[i].Response,
                     "message":feat[i].Message
             });
         }
