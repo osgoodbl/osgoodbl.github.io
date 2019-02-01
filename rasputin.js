b103e61f9a5c1caba995b83d4a247ed622a515f4
Brian Osgood <35780331+osgoodbl@users.noreply.github.com>
2019-02-01 13:11:49 -0800

    Update rasputin.js

--- rasputin.js
+++ rasputin.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
           "X-API-Key": apiKey
         }
       }).done(function(activity){
-     console.log(activity);
+     console.log(activity, membershipType, gamertag);
       });
     });
   });
