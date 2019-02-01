3ceabf937a6c7b377ad7087859ca606d82254524
Brian Osgood <35780331+osgoodbl@users.noreply.github.com>
2019-02-01 11:15:29 -0800

    Update rasputin.js

--- rasputin.js
+++ rasputin.js
@@ -1,12 +1,15 @@
 var apiKey = "668b2e3fc37a47de87d3b5e29defacd9";
 
 $(document).ready(function() {
-    $.ajax({
+  $("#profilebutton").click(function() {
+      $.ajax({
         url: "https://www.bungie.net/platform/Destiny2/SearchDestinyPlayer/2/vicodin4000/",
         headers: {
           "X-API-Key": apiKey
         }
           }).done(function(json){
      console.log(json);
-    });
+              var gamertag = json.Response[0].displayName
+        });
+      });
 });
