b5c94449a9ce433b3b6fa467e774b44ae6b0aa21
Brian Osgood <35780331+osgoodbl@users.noreply.github.com>
2019-02-01 13:09:57 -0800

    Update rasputin.js

--- rasputin.js
+++ rasputin.js
@@ -1,5 +1,7 @@
 var apiKey = "668b2e3fc37a47de87d3b5e29defacd9";
-
+var gamertag;
+var membershipId;
+var membershipType;
 $(document).ready(function() {
   $("#profilebutton").click(function() {
       $.ajax({
@@ -9,13 +11,9 @@ $(document).ready(function() {
         }
       }).done(function(player){
      console.log(player);
-     tableData = [];
-         tableData.push({
-             "gamertag": player.Response[0].displayName,
-             "membershipId":player.Response[0].membershipId,
-             "membershipType":player.Response[0].membershipType
-        });
-        return tableData
+     gamertag = player.Response[0].displayName;
+     membershipId = player.Response[0].membershipId;
+     membershipType = player.Response[0].membershipType;
       });
   $("#activitybutton").click(function() {
       $.ajax({
